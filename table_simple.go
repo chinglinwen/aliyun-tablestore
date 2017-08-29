9f69466bb5a5c20cb0877de80238ca369c74743b
wenzhenglin <wenzhenglin@haodai.net>
2017-08-29 10:27:18 +0800

    Update.

--- table_simple.go
+++ table_simple.go
@@ -13,77 +13,130 @@ var TagName = "tablestore"
 
 // Simple table from struct model.
 func NewSimpleTable(s interface{}, options ...tableOption) (*Table, error) {
-	name := nameConvert(structName(s))
+	name, err := structName(s)
+	if err != nil {
+		return nil, err
+	}
 	row, err := structToRow(s)
 	if err != nil {
 		return nil, err
 	}
-	return New(name, []Row{row}, options...)
+	return New(name, []Row{row}, options...), nil
 }
 
 func NewSimpleTableBatch(ss []interface{}, options ...tableOption) (*Table, error) {
-	name := nameConvert(structName(s))
+	name, err := structName(ss)
+	if err != nil {
+		return nil, err
+	}
 	rows := []Row{}
 	for i, s := range ss {
 		row, err := structToRow(s)
 		if err != nil {
-			return nil, errors.New(fmt.Sprintf("item %v: %v", i, err))
+			return nil, fmt.Errorf("item %v: %v", i, err)
 		}
 		rows = append(rows, row)
 	}
-	return New(name, rows, options...)
+	return New(name, rows, options...), nil
 }
 
 // CreateSimpleTable create the simple table directly.
 func CreateSimpleTable(s interface{}) error {
-	return NewSimpleTable(s).Create()
+	t, err := NewSimpleTable(s)
+	if err != nil {
+		return err
+	}
+	return t.Create()
 }
 
 // For multiple rows as batch process.
 func CreateSimpleTableBatch(ss []interface{}) error {
-	return NewSimpleTableBatch(ss).Create()
+	t, err := NewSimpleTableBatch(ss)
+	if err != nil {
+		return err
+	}
+	return t.Create()
 }
 
 func GetRow(s interface{}) (Row, error) {
-	return NewSimpleTable(s).GetRow()
+	t, err := NewSimpleTable(s)
+	if err != nil {
+		return nil, err
+	}
+	return t.GetRow()
 }
 
 func GetRowHistory(s interface{}, max int) (RowHistory, error) {
-	return NewSimpleTable(s).GetRowHistory(max)
+	t, err := NewSimpleTable(s)
+	if err != nil {
+		return nil, err
+	}
+	return t.GetRowHistory(max)
 }
 
 func PutRow(s interface{}) error {
-	return NewSimpleTable(s).PutRow()
+	t, err := NewSimpleTable(s)
+	if err != nil {
+		return err
+	}
+	return t.PutRow()
 }
 
 func UpdateRow(s interface{}) error {
-	return NewSimpleTable(s).UpdateRow()
+	t, err := NewSimpleTable(s)
+	if err != nil {
+		return err
+	}
+	return t.UpdateRow()
 }
 
 func GetRows(ss []interface{}) ([]Row, error) {
-	return NewSimpleTableBatch(ss).GetRows()
+	t, err := NewSimpleTableBatch(ss)
+	if err != nil {
+		return nil, err
+	}
+	return t.GetRows()
 }
 
 func PutRows(ss []interface{}) error {
-	return NewSimpleTable(ss).PutRows()
-}
-
-func structName(s interface{}) string {
-	return reflect.TypeOf(s).Name()
-}
-
-func structToRow(s interface{}) (Row, error) {
-	t := reflect.TypeOf(s)
-	if t.Kind() != "struct" {
-		return nil, errors.New("type is not struct")
+	t, err := NewSimpleTableBatch(ss)
+	if err != nil {
+		return err
 	}
-	row := []Column{}
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		value := field.Interface()
-		name := nameConvert(field.Name)
-		tag := field.Tag.Get(TagName)
-		if tag != nil {
+	return t.PutRows()
+}
+
+func structName(s interface{}) (string, error) {
+	v, err := strctVal(s)
+	if err != nil {
+		return "", err
+	}
+	return nameConvert(v.Type().Name()), nil
+}
+
+func structToRow(s interface{}) (row Row, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+			return
+		}
+	}()
+
+	v, err := strctVal(s)
+	if err != nil {
+		return nil, err
+	}
+	row = []Column{}
+	for i := 0; i < v.NumField(); i++ {
+		// skip unexported field
+		typeField := v.Type().Field(i)
+		if unicode.IsLower(rune(typeField.Name[0])) {
+			continue
+		}
+
+		tag := typeField.Tag.Get(TagName)
+		name := nameConvert(typeField.Name)
+		if tag != "" {
 			tags := strings.Split(tag, ",")
 			if tags[0] == "-" {
 				continue // ignore this column
@@ -93,9 +146,23 @@ func structToRow(s interface{}) (Row, error) {
 			}
 		}
 		pkey := strings.Contains(tag, "pkey")
+		value := v.Field(i).Interface()
 		row = append(row, Column{Name: name, Value: value, Pkey: pkey})
 	}
 	return row, nil
+}
+
+func strctVal(s interface{}) (v reflect.Value, err error) {
+	v = reflect.ValueOf(s)
+	// if pointer get the underlying element≤
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if v.Kind() != reflect.Struct {
+		err = errors.New("not a struct")
+		return
+	}
+	return
 }
 
 func nameConvert(name string) string {
@@ -110,7 +177,3 @@ func nameConvert(name string) string {
 	}
 	return strings.ToLower(strings.Join(words, "_"))
 }
-
-// use slice as row
-
-// use struct as row
--- table_simple_test.go
+++ table_simple_test.go
@@ -1,18 +1,64 @@
 package tablestore
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
-type user struct {
-	id   int
-	user string
-	pass string
+type User struct {
+	Id   int    `tablestore:",pkey"`
+	User string `tablestore:"usera"`
+	Pass string
+
+	extra string // `tablestore:"-"`
 }
 
-var u = user{id: 1, user: "user"}
+var u = User{Id: 1, User: "user1"}
 
 func init() {
-	err := CreateKV(kvname, "", "", SetMaxVersion(10))
+	err := CreateSimpleTable(u)
 	if err != nil {
-		fmt.Println("create kv err ", err)
+		fmt.Println("create simple table err ", err)
+	}
+}
+
+func TestSimpleTableName(t *testing.T) {
+	name, err := structName(u)
+	if err != nil {
+		t.Errorf("err %v", err)
+	}
+	expect := "user"
+	if name != expect {
+		t.Errorf("table name got %q, expect %q", name, expect)
+	}
+}
+
+func TestSimplePutRow(t *testing.T) {
+	err := PutRow(u)
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
+}
+
+func TestSimpleUpdateRow(t *testing.T) {
+	err := UpdateRow(u)
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
+}
+
+func TestSimpleGetRow(t *testing.T) {
+	row, err := GetRow(u)
+	if err != nil {
+		t.Errorf("err: %v", err)
+		return
+	}
+	printRow(row)
+}
+
+func TestDelSimple(t *testing.T) {
+	err := DelTable("user")
+	if err != nil {
+		fmt.Println("err: %v", err)
 	}
 }
